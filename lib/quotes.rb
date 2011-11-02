be8e58bb782cf5671c9e4b13c1125209ad80ce61
R. Scott Shaffer <scott.shaffer@t-mobile.com>
2011-11-02 10:19:20 -0700

    quotes_spec - all examples pass

--- lib/quotes.rb
+++ lib/quotes.rb
@@ -16,6 +16,61 @@
 # @see http://rubydoc.info/stdlib/core/1.9.2/Module#alias_method-instance_method
 # 
 class Quotes
+
+  DEFAULT_QUOTE = "Could not find a quote at this time"
+
+  class << self
+    attr_accessor :missing_quote
+  end
+
+  self.missing_quote = DEFAULT_QUOTE
+
+  def self.load file
+    Quotes.new(file: file)
+  end
+
+
+  attr_reader :file, :quotes
+ 
+  alias_method  :all,  :quotes
+  
+  def initialize parms
+    @file = parms[:file]
+    @quotes = all_quotes @file
+  end
+  
+  def find index
+    @quotes.empty? ? Quotes.missing_quote : (0...quotes.size).include?(index) ? @quotes[index] : quotes.sample
+  end
+
+  alias_method  :[], :find
+
+  def search parms = {} 
+    results = parms.map {|criterion| @quotes.grep( create_regexp(*criterion) )}.flatten
+    results.empty? ? @quotes : results
+  end
   
   
+  private
+  
+  #
+  # @param [String] filename the name of the file to load all the quotes.
+  #
+  # @return [Array<String>] all the quotes found in the file.
+  #
+  def all_quotes(filename)
+    if File.exists? filename
+      File.readlines(filename).map {|quote| quote.strip }
+    else
+      []
+    end
+  end
+
+  def create_regexp criterion, text
+    case criterion
+      when :start_with then %r(^#{text})
+      when :end_with   then %r(#{text}$)
+      when :include    then %r(#{text})
+    end
+  end
 end
--- spec/quotes_spec.rb
+++ spec/quotes_spec.rb
@@ -1,166 +1,118 @@
 require_relative 'spec_helper'
 
-
 describe Quotes do
     
   subject { Quotes.new :file => TEXT_FILE }
   
   describe "#file" do
-    
     it "should return the filename" do
-      pending
       subject.file.should == TEXT_FILE
     end
-    
   end
   
+  
   describe "#all", "#quotes" do
-    
     it "should return all the quotes" do
-      pending
       subject.all.should_not be_empty
       subject.quotes.should_not be_empty
     end
-    
   end
   
+  
   describe "#[]", "#find" do
-
+  
     context "when the file does not exist" do
-
       subject { Quotes.new :file => "FILE_DOES_NOT_EXIST" }
 
       it "should return an empty list of results" do
-        pending
         subject[3].should == "Could not find a quote at this time"
         subject.find(3).should == "Could not find a quote at this time"
       end
-
     end
 
     context "when the file exists" do
 
       context "when a single line has been specified" do
-
         it "should return the correct value at the specified line" do
-        pending
           subject[1].should == "The dude abides"
           subject.find(1).should == "The dude abides"
         end
 
         context "when the line specified is greater than the number of quotes" do
-
           it "should return any quote" do
-            pending
             subject[999].should_not be_nil
             subject.find(999).should_not be_nil
           end
-
         end
-
       end
-
     end
-
   end
   
+  
   describe "#search" do
-    
+  
     context "when the file does not exist" do
-
       subject { Quotes.new :file => "FILE_DOES_NOT_EXIST" }
 
       it "should return an empty list of results" do
-        pending
         subject.search(:start_with => "The").should be_empty
       end
-
     end
 
     context "when the file does exist" do
 
       context "when no search criteria has been specified" do
-
         it "should return all quotes" do
-          pending
           subject.search.should_not be_empty
         end
-
       end
 
       context "when search criteria has been specified" do
 
         describe "contains" do
-
           it "should return all the quotes that contain the criteria" do
-            pending
             results = subject.search(:include => "dude")
             results.should have(3).items
           end
-
         end
 
-        describe "starts with" do
-
+        describe "start with" do
           it "should return all the quotes that contain the criteria" do
-            pending
             results = subject.search(:start_with => "Her")
-
             results.should have(1).items
             results.first.should == "Her life is in your hands dude"
           end
-
         end
 
-        describe "ends with" do
-
+        describe "end with" do
           it "should return all the quotes that contain the criteria" do
-            pending
             results = subject.search(:end_with => "dude")
-
             results.should have(2).items
             results.should == [ 'Calmer than you are dude', 'Her life is in your hands dude' ]
-
           end
-
         end
       end
-
     end
-    
   end
+  
   
   describe "Class Methods" do
     
     describe "::load" do
-      
-      it "should create a quotes objects with a file" do
-        pending
+      it "should create a quotes object with a file" do
         quotes = Quotes.load(TEXT_FILE)
         quotes.all.should_not be_empty
       end
-      
     end
     
     describe "::missing_quote" do
-      
-      
       it "should allow you to set the message when a quote is missing" do
-        pending
-        
         expected_quote = "All of our quotes have gone missing"
-        
         Quotes.missing_quote = expected_quote
         
         quotes = Quotes.load("FILE_DOES_NOT_EXIST")
         quotes[3].should == expected_quote
-        
       end
-      
-      
     end
-    
-    
   end
-  
 end
